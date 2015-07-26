3ad4ef00644f79eebcdc00f93f66310f93da8a1c
Styden <lilocolson@windowslive.com>
2015-07-26 16:27:46 +0300

    En dévelopement

--- main.c
+++ main.c
@@ -16,12 +16,6 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
-
-void uploadingjbdata() {
-    system("cd /tmp; cd rootun; ./afcclient mkdir rootun-install");
-    /*system("cd /tmp; cd rootun; ./afcclient put /tmp/rootun/Cydia.tar /rootun-install/Cydia.tar");
-    system("cd /tmp; cd rootun; ./afcclient put /tmp/rootun/packagelist.tar /rootun-install/packagelist.tar");
-    system("cd /tmp; cd rootun; ./afcclient put /tmp/rootun/extras.tar /rootun-install/extras.tar");*/
 }
 void downloadapp() {
     system("cd /tmp; mkdir -p WWDC");
@@ -51,35 +45,7 @@ void downloadapp() {
     fclose(tmp);
     curl_easy_cleanup(curl);
 }
-void downloadutilities() {
-    CURL *curl;
-    CURLcode curl_res;
-    CURLINFO info;
-    long http_code;
-    double c_length;
-    FILE *tmp;
-    tmp=fopen("/tmp/rootun.zip", "w");
-    if(tmp==NULL) {
-        printf("Error to download file\n");
-        exit(2);
-    }
-    curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_URL, "http://3x7r00tripper.team-sync.info/rootun.zip");
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, tmp);
-    curl_res = curl_easy_perform(curl);
-    if(curl_res==0) {
-        printf(" [*] Utilities downloaded successfully\n");
-    } else {
-        printf("ERROR in dowloading file\n");
-        fclose(tmp);
-        curl_easy_cleanup(curl);
-    }
-    fclose(tmp);
-    curl_easy_cleanup(curl);
-}
-void setuputilities() {
-    system("cd /tmp; unzip rootun.zip; cd rootun; make");
-}
+
 void injectionapp1() {
     system("cd /tmp/rootun; ./afcclient mkdir Downloads/WWDC.app; ./afcclient put /tmp/WWDC/Payload/WWDC.app/WWDC Downloads/WWDC.app/WWDC");
     system("cd /tmp/rootun; ./afcclient put /tmp/rootun/jailbreak.png /rootun-install/jailbreak.png");
@@ -92,17 +58,10 @@ void injectionapp2() {
     
 }
 int main() {
-    printf(" [*] Starting iOS7 untethered jailbreak!\n");
-    printf(" [*] Thank you for using Rootun by @3x7R00Tripper.\n");
-    printf(" [*] For a GUI use Syncun by @3x7R00Tripper wich use the same code than Rootun.\n");
+    printf(" [*] Starting Installation of personnalized app !\n");
+    printf(" [*] Thank you for using F4k3r1P4 From Lilo Colson (@Pwn1d).\n");
     printf(" [*] Retrieving remote package...\n");
     downloadapp();
-    printf(" [*] Downloading Utilities\n");
-    downloadutilities();
-    printf(" [*] Setup Utilities\n");
-    setuputilities();
-    printf(" [*] Uploading Jailbreak Data\n");
-    uploadingjbdata();
     printf(" [*] Unpacking WWDC\n");
     printf(" [*] Injecting rootun app (1/2)\n");
     injectionapp1();
